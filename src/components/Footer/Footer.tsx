726b9f569ecaf569bce668173bebd9b3f169a3b3
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-08-17 13:13:16 +0200

    feat: created Footer, added styles, images

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import GitHub from './assets/GitHub.png';
+import RSSchool from './assets/Rss_Logo.svg';
+import footer from './Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>@2022 RSLang</p>
+      <div className={footer.gitHub_link}>
+        <a href="https://github.com/ivnpotapov">
+          <img src={GitHub} alt="Logo GitHub" className={footer.github} />
+          <p>ivnpotapov</p>
+        </a>
+        <a href="https://github.com/kate-js">
+          <img src={GitHub} alt="Logo GitHub" className={footer.github} />
+          <p>kate-js</p>
+        </a>
+        <a href="https://github.com/glsidorenko">
+          <img src={GitHub} alt="Logo GitHub" className={footer.github} />
+          <p>glsidorenko</p>
+        </a>
+      </div>
+      <a href="https://rs.school/">
+        <img src={RSSchool} alt="Logo RSSchool" className={footer.rss_school} />
+      </a>
+    </footer>
+  );
+};
