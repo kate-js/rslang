28d77ecfbcc28affa70b8ca3c0a3928d3e55da16
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-08-17 12:56:10 +0200

    refactor: update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { About } from './pages/About';
-import { Audio } from './pages/Audio';
-import { Games } from './pages/Games';
-import { Home } from './pages/Home';
-import { NotFound } from './pages/NotFound';
-import { Sprint } from './pages/Sprint';
-import { Layout } from './components/Layout';
+import { Layout } from './components/Layout/Layout';
+import { About } from './pages/About/About';
+import { Audio } from './pages/Audio/Audio';
+import { Games } from './pages/Games/Games';
+import { Home } from './pages/Home/Home';
+import { NotFound } from './pages/NotFound/NotFound';
+import { Sprint } from './pages/Sprint/Sprint';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/games" element={<Games />} />
-          <Route path="/games/audio" element={<Audio />} />
-          <Route path="/games/sprint" element={<Sprint />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/games" element={<Games />} />
+            <Route path="/games/audio" element={<Audio />} />
+            <Route path="/games/sprint" element={<Sprint />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
