aabd14f379c4ea27b5deb6c0e64ef9365fbaa741
Gleb <glsidorenko01@gmail.com>
2022-08-17 20:04:11 +0300

    feat: add content and styles

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,3 +1,33 @@
+import styles from './About.module.css'
+import { TeamMember } from '../TeamMember';
+import image from '../assets/member-photo.png'
+
 export const About = () => {
-  return <div>Здесь будет информаци о нашей команде</div>;
+  return (
+    <div className={styles.about}>
+      <h1 className={styles.title}>Над проектом работали</h1>
+      <div className={styles.team}>
+        <TeamMember 
+          img={image} 
+          name={'Иван Потапов'} 
+          role={'Team Lead'} 
+          description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+        />
+
+        <TeamMember 
+          img={image} 
+          name={'Екатерина Вакульская'} 
+          role={'Developer'}
+          description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+        />
+
+        <TeamMember 
+          img={image} 
+          name={'Глеб Сидоренко'} 
+          role={'Developer'} 
+          description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+        />
+      </div>
+    </div>
+  )
 };
