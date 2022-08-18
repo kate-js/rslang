17b14ade23c4a74ce25fd17a79e042f7a5d0f50a
Gleb <glsidorenko01@gmail.com>
2022-08-18 20:01:47 +0300

    refactor: add generation according data array

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,36 +1,37 @@
 import styles from './About.module.css'
-
 import { TeamMember } from './TeamMember';
 import image from '../../assets/member-photo.png'
+
+const membersData = [
+  { 
+    img: image, 
+    name: 'Иван Потапов', 
+    role: 'Team Lead', 
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ', 
+    link: 'https://github.com/ivnpotapov'},
+  { 
+    img: image, 
+    name: 'Екатерина Вакульская', 
+    role: 'Developer', 
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    link: 'https://github.com/kate-js',
+  },
+  { 
+    img: image, 
+    name: 'Глеб Сидоренко', 
+    role: 'Developer', 
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', 
+    link: 'https://github.com/glsidorenko'},
+];
 
 export const About = () => {
   return (
     <div className={styles.about}>
       <h1 className={styles.title}>Над проектом работали</h1>
       <div className={styles.team}>
-        <TeamMember 
-          img={image} 
-          name={'Иван Потапов'} 
-          role={'Team Lead'} 
-          description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
-          link={'https://github.com/ivnpotapov'}
-        />
-
-        <TeamMember 
-          img={image} 
-          name={'Екатерина Вакульская'} 
-          role={'Developer'}
-          description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
-          link={'https://github.com/kate-js'}
-        />
-
-        <TeamMember 
-          img={image} 
-          name={'Глеб Сидоренко'} 
-          role={'Developer'} 
-          description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
-          link={'https://github.com/glsidorenko'}
-        />
+        {membersData.map((member, index) => {
+          return <TeamMember key={index + 1} {...member}/>
+        })}
       </div>
     </div>
   )
