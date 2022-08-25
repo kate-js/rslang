cdc278a77dc7688bb50457e8e47a18170ce48836
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-08-25 12:46:39 +0200

    refactor: logic changed

--- src/pages/Tutorial/Tutorial.tsx
+++ src/pages/Tutorial/Tutorial.tsx
@@ -1,45 +1,29 @@
 import { useState } from 'react';
 import styles from './Tutorial.module.css';
-import { MouseEvent } from 'react';
-import { Tutorial_description } from './Tutorial.description.tsx/Tutorial_description';
-import { cardTutorial } from '../../utils/constants';
-import { fontPageTutorial, LEVELS_main } from '../../data/Data';
+import { LEVELS_main } from '../../data/Data';
 import { Link } from 'react-router-dom';
+import { LevelDescription } from './LevelDescription/LevelDescription';
 
 export const Tutorial = () => {
-  const [level, setLevel] = useState('A1');
+  const [level, setLevel] = useState(0);
 
-  function updateLevel(event: MouseEvent) {
-    const level = (event.target as HTMLElement).innerHTML;
-    setLevel(level);
+  function updateLevel(index: number) {
+    setLevel(index);
   }
 
   return (
     <div className={styles.tutorial}>
       <ul className={styles.levels}>
         {LEVELS_main.map((item, index) => (
-          <li key={index}>
-            <Link to={`./${item}`} className={styles.link} onMouseOver={updateLevel}>
+          <li key={index} value={item} onMouseOver={() => updateLevel(index)}>
+            <Link to={`./${item}`} className={styles.link}>
               {item}
             </Link>
           </li>
         ))}
       </ul>
       <>
-        {fontPageTutorial.map((item: cardTutorial) =>
-          item.name === level ? (
-            <Tutorial_description
-              key={item.id}
-              id={item.id}
-              name={item.name}
-              bigName={item.bigName}
-              url={item.url}
-              description={item.description}
-            />
-          ) : (
-            console.log('fvev')
-          )
-        )}
+        <LevelDescription level={level} />
       </>
     </div>
   );
