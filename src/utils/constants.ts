27bb3e8b208007c53750b099292b70f58734ab0b
Gleb <glsidorenko01@gmail.com>
2022-08-27 00:55:03 +0300

    refactor: move interfaces and fuctions

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,3 +8,45 @@ export enum ERoutes {
   tutorial = '/tutorial',
   notFound = '*'
 }
+
+export interface IWord {
+  id: string,
+  group: number,
+  page: number,
+  word: string,
+  image: string,
+  audio: string,
+  audioMeaning: string,
+  audioExample: string,
+  textMeaning: string,
+  textExample: string,
+  transcription: string,
+  textExampleTranslate: string,
+  textMeaningTranslate: string,
+  wordTranslate: string,
+  correct?: boolean,
+}
+
+export interface IState {
+  isLoaded: boolean,
+  words: IWord[];
+}
+
+export interface IVolumeSettings {
+  volume: number,
+  isMuted: boolean,
+}
+
+export interface IComponentState {
+  answerCount?: number,
+  volumeSettings?: IVolumeSettings,
+  playing?: boolean,
+  answer?: IWord,
+  answers?: IWord[],
+  audio?: HTMLAudioElement,
+  newWords?: IWord[],
+  appState?: IState,
+  isAnswered?: boolean
+}
+
+export type keys = 'appState' | 'isAnswered' | 'newWords' | 'answer' | 'audio' | "answerCount" | "volumeSettings";
