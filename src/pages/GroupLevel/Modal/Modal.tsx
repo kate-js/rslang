82166d1213a631d26a47fe8cd94c566b3a3f0ce2
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-08-31 00:31:02 +0200

    refactor: updated style for modal window

--- src/pages/GroupLevel/Modal/Modal.tsx
+++ src/pages/GroupLevel/Modal/Modal.tsx
@@ -6,6 +6,7 @@ import { useSelector } from 'react-redux';
 import { TState } from '../../../store/store';
 import { useEffect, useState } from 'react';
 import buttonStyles from '../../../components/UI/Button/Button.module.css';
+import Hard from '../assets/hard-word.png';
 
 export const Modal = ({ modal, setModal, word, hard, learn }: IModal) => {
   const token = useSelector((state: TState) => state.auth.currentUser.token);
@@ -79,50 +80,54 @@ export const Modal = ({ modal, setModal, word, hard, learn }: IModal) => {
     <div className={rootClasses.join(' ')} onClick={() => setModal(false)}>
       <div className={styles.ModalContent} onClick={(e) => e.stopPropagation()}>
         <div className={styles.modal_main}>
-          <h1>{word?.word}</h1>
-          <p>{word?.transcription}</p>
-          <h3>{word?.wordTranslate}</h3>
+          <div className={styles.modal_title}>
+            <h1>{word?.word}</h1>
+            <p>{word?.transcription}</p>
+            <h3>{word?.wordTranslate}</h3>
+            {hardWord ? <img src={Hard} alt="mark-hard-word" className={styles.hard_word} /> : null}
+          </div>
           {word?.image ? <img src={`${EApiParametrs.baseUrl}/${word?.image}`} alt="" /> : null}
-          {hardWord ? <p>Внимание! Слово отмечено как сложное!</p> : null}
         </div>
-        <div className={styles.modal_examples}>
-          <div className={styles.modal_example}>
-            <p>{word?.textExample}</p>
-            <p>{word?.textExampleTranslate}</p>
-            {word?.audioExample ? (
-              <ReactAudioPlayer src={`${EApiParametrs.baseUrl}/${word?.audioExample}`} controls />
-            ) : null}
-          </div>
-          <div className={styles.modal_example}>
-            <p>{word?.textMeaning}</p>
-            <p>{word?.textMeaningTranslate}</p>
-            {word?.audioMeaning ? (
-              <ReactAudioPlayer src={`${EApiParametrs.baseUrl}/${word?.audioMeaning}`} controls />
-            ) : null}
-          </div>
-          {isLodined ? (
-            <div>
-              {!hardWord ? (
-                <button onClick={sendHardWord} className={buttonStyles.button}>
-                  Добавить в сложные
-                </button>
-              ) : (
-                <button onClick={deleteHardWord} className={buttonStyles.button}>
-                  Удалить из сложного
-                </button>
-              )}
-              {!learnWord ? (
-                <button onClick={addLearnWord} className={buttonStyles.button}>
-                  Пометить как изученное
-                </button>
-              ) : (
-                <button onClick={deleteLearningWord} className={buttonStyles.button}>
-                  Удалить из изученного
-                </button>
-              )}
+        <div>
+          <div className={styles.modal_examples}>
+            <div className={styles.modal_example}>
+              <p>{word?.textExample}</p>
+              <p>{word?.textExampleTranslate}</p>
+              {word?.audioExample ? (
+                <ReactAudioPlayer src={`${EApiParametrs.baseUrl}/${word?.audioExample}`} controls />
+              ) : null}
             </div>
-          ) : null}
+            <div className={styles.modal_example}>
+              <p>{word?.textMeaning}</p>
+              <p>{word?.textMeaningTranslate}</p>
+              {word?.audioMeaning ? (
+                <ReactAudioPlayer src={`${EApiParametrs.baseUrl}/${word?.audioMeaning}`} controls />
+              ) : null}
+            </div>
+          </div>
         </div>
+        {isLodined ? (
+          <div>
+            {!hardWord ? (
+              <button onClick={sendHardWord} className={buttonStyles.button}>
+                Добавить в сложные
+              </button>
+            ) : (
+              <button onClick={deleteHardWord} className={buttonStyles.button}>
+                Удалить из сложного
+              </button>
+            )}
+            {!learnWord ? (
+              <button onClick={addLearnWord} className={buttonStyles.button}>
+                Пометить как изученное
+              </button>
+            ) : (
+              <button onClick={deleteLearningWord} className={buttonStyles.button}>
+                Удалить из изученного
+              </button>
+            )}
+          </div>
+        ) : null}
       </div>
     </div>
   );
