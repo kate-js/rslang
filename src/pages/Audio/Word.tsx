2d1cb4d15d1028cdcaefbd1e7dca1d15d46c7ba5
Gleb <glsidorenko01@gmail.com>
2022-08-31 16:35:47 +0300

    refactor: change prop name

--- src/pages/Audio/Word.tsx
+++ src/pages/Audio/Word.tsx
@@ -1,11 +1,16 @@
 import styles from './Word.module.css'
 import AudioImg from './assets/audio.png'
 
-export const Word = ({id, word, wordTranslate, func}) => {
+export const Word = ({id, word, wordTranslate, playSound}) => {
   return(
     <div className={styles.word} key={id}>
-      <img src={AudioImg} alt="Audio Image" className={styles.audio__overall} onClick={func}/>
+      <img 
+        src={AudioImg} 
+        alt="Audio Image" 
+        className={styles.audio__overall} 
+        onClick={playSound}
+      />
       <span key={id}>{`${word}`} - {wordTranslate}</span>
-      </div>
+    </div>
   )
 }
