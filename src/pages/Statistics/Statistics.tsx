609fcb905c95846051d63d929b4628b632ddda06
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-09-03 11:33:36 +0200

    feat: created statistic page

--- src/pages/Statistics/Statistics.tsx
+++ src/pages/Statistics/Statistics.tsx
@@ -1,28 +1,31 @@
-import React, { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { incremented, decremented, setCounter } from '../../store/testToolkitSlice';
-import { TState } from '../../store/store';
+import { Statistics_title } from '../../data/Data';
+import styles from './Statistics.module.css';
 
 export const Statistics = () => {
-  const counter = useSelector((state: TState) => state.testToolkit.value);
-  const dispatch = useDispatch();
-
-  const increment = useCallback(() => dispatch(incremented()), []);
-  const decrement = useCallback(() => dispatch(decremented()), []);
-  const asyncDecrement = useCallback(async () => {
-    setTimeout(() => dispatch(decremented()), 1500);
-  }, []);
-  const asyncSetCounter = useCallback(async () => {
-    setTimeout(() => dispatch(setCounter(5)), 1500);
-  }, []);
-
   return (
-    <div>
-      <p>{counter}</p>
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
-      <button onClick={asyncDecrement}>asyncDecrement</button>
-      <button onClick={asyncSetCounter}>asyncSetCounter</button>
+    <div className={styles.statictics_section}>
+      <h1>Краткосрочная статистика</h1>
+      <ul className={styles.statictics_titles}>
+        {Statistics_title.map((item, index) => (
+          <li className={styles.statictics_title} key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+      <ul className={styles.statictics_items}>
+        <li className={styles.statictics_item}>
+          <p>Количество новых слов за день</p>
+          <div className={styles.statictics_title}>19</div>
+        </li>
+        <li className={styles.statictics_item}>
+          <p>Процент правильных ответов</p>
+          <div className={styles.statictics_title}>20</div>
+        </li>
+        <li className={styles.statictics_item}>
+          <p>Самая длинная серия правильных ответов</p>
+          <div className={styles.statictics_title}>21</div>
+        </li>
+      </ul>
     </div>
   );
 };
