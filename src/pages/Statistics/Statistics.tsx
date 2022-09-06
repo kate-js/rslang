4192c020c190f3531bdb44ed8f537f7d1902535f
Katsiaryna Vakulskaya <kate.vakulskaya@gmail.com>
2022-09-06 14:09:24 +0200

    feat: add calculated total statistics

--- src/pages/Statistics/Statistics.tsx
+++ src/pages/Statistics/Statistics.tsx
@@ -8,10 +8,9 @@ import styles from './Statistics.module.css';
 export const Statistics = () => {
   const [audio, setAudio] = useState<IOptional>();
   const [sprint, setSprint] = useState<IOptional>();
-  const [total, setTotal] = useState<IOptional>();
   const [audioDay, setAudioDay] = useState<number>();
   const [sprintDay, setSprintDay] = useState<number>();
-  const [totalDay, setTotalDay] = useState<number>();
+  const [totalDay, setTotalDay] = useState<number>(0);
   const userId = useSelector((state: TState) => state.auth.currentUser.userId);
 
   useEffect(() => {
@@ -24,25 +23,22 @@ export const Statistics = () => {
   async function getStat() {
     try {
       const response = await apiUsersStatistic.getStatistics(userId);
-      const { audio, sprint, total } = response.optional;
-      setAudio(audio);
-      setTotal(total);
-      setSprint(sprint);
+      const { audio, sprint } = response.optional;
+      await setAudio(audio);
+      await setSprint(sprint);
 
       const arr = sprint?.learnNewWordPerDay;
       arr.map((item, index) => {
         date == sprint?.learnNewWordPerDay[index].date;
         setSprintDay(sprint?.learnNewWordPerDay[index].counter);
+        setTotalDay(
+          sprint?.learnNewWordPerDay[index].counter + audio?.learnNewWordPerDay[index].counter
+        );
       });
       const arr1 = audio?.learnNewWordPerDay;
       arr1.map((item, index) => {
         date == audio?.learnNewWordPerDay[index].date;
         setAudioDay(audio?.learnNewWordPerDay[index].counter);
-      });
-      const arr2 = total?.learnNewWordPerDay;
-      arr2.map((item, index) => {
-        date == total?.learnNewWordPerDay[index].date;
-        setTotalDay(total?.learnNewWordPerDay[index].counter);
       });
     } catch (error) {
       console.error(error);
@@ -95,11 +91,20 @@ export const Statistics = () => {
             </li>
             <li className={styles.statictics_item}>
               <p>Процент правильных ответов</p>
-              <div className={styles.statictics_title}>{total?.percentRigth.right}</div>
+              <div className={styles.statictics_title}>
+                {(Math.round(
+                  ((sprint?.percentRigth.right as number) + (audio?.percentRigth.right as number)) /
+                    2
+                ) as number) | 0}
+              </div>
             </li>
             <li className={styles.statictics_item}>
               <p>Самая длинная серия правильных ответов</p>
-              <div className={styles.statictics_title}>{total?.longestStrick}</div>
+              <div className={styles.statictics_title}>
+                {(sprint?.longestStrick as number) > (audio?.longestStrick as number)
+                  ? sprint?.longestStrick
+                  : audio?.longestStrick}
+              </div>
             </li>
           </ul>
         </div>
